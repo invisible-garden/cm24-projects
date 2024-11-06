dff2597cc9862d64b0be946bc21286b64370626f
Alex Padilla <alex.padilla@epn.edu.ec>
2024-11-06 10:03:53 -0500

    Remove other files

--- web3-lms/config/moodle/moodle_db-setup/create_wallet_field.sql
+++
@@ -1,4 +0,0 @@
--- db-setup/create_wallet_field.sql
-
-INSERT INTO mdl_user_info_field (shortname, name, datatype, categoryid, description, descriptionformat, required, locked, visible, forceunique, signup, defaultdata, defaultdataformat, param1, param2, param3)
-VALUES ('wallet', 'Ethereum Wallet', 'text', 1, 'User Ethereum wallet address', 1, 0, 0, 1, 0, 0, '', 1, 42, '', '');
